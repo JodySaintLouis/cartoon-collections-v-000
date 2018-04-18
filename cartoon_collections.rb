19b45661592a8e74e62e224211c3e53a571cfdbe
Jamal Gelin <gelin.jamal@gmail.com>
2018-04-18 02:24:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,6 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
 planeteer_calls.map! { |i| i + "!"}
 planeteer_calls.map! { |i |i.capitalize}
-
 end
 
 def long_planeteer_calls(long_planeteer_calls)# code an argument here

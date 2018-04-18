041ab0b6b24a9987ed9ee209fc7703fb06eaae68
Jamal Gelin <gelin.jamal@gmail.com>
2018-04-18 02:37:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 
 def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda" ""]
+  cheese_types = ["cheddar", "gouda" "camembert"]
 end

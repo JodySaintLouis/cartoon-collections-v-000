5da2cc7ff226cdf00c7b4eb44148ce1e237f1974
Jamal Gelin <gelin.jamal@gmail.com>
2018-04-18 02:01:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index { |dwarves, index|
-    hash[dwarves] = inded
+    hash[dwarves] = index
   }
 end
 

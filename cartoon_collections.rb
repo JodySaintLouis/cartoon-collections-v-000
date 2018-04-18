96eb624c25aed87a657c1c330e57239f5cafa111
Jamal Gelin <gelin.jamal@gmail.com>
2018-04-18 01:11:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,10 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-dwarves = ["Doc", "Dopey", "Bashful", "Grump"]
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 end
 
 def long_planeteer_calls(long_planeteer_calls)# code an argument here

60b3d266b1abe75bf2f885d8152b5d96c166dfee
Jamal Gelin <gelin.jamal@gmail.com>
2018-04-18 02:57:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |name, i|
     puts "#{i + 1}. #{name}"
    end
-
+end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
